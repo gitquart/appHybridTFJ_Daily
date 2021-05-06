5366f6b0157e459dd3326de2cc02f68fa1e079bf
Ulyses <ulyses.rico.2012@gmail.com>
2021-05-06 14:24:19 -0500

    ...

--- apptfj_daily_3/InternalControl.py
+++ apptfj_daily_3/InternalControl.py
@@ -1,12 +1,12 @@
 import os
 
 class cInternalControl:
-    idControl=7
+    idControl=8
     timeout=70
     heroku=True
     pdfOn=False
     download_dir='Download_tfja_Daily'
-    hfolder='apptfj_daily_2'   
+    hfolder='apptfj_daily_3'   
     rutaHeroku='/app/'+hfolder
     rutaLocal=os.getcwd()+'\\'+hfolder+'\\'
     
